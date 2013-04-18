f6a6732b277e15e571148aad3ed409d0075e0670
miker169 <miker169@hotmail.co.uk>
2013-04-18 19:01:18 +0100

    Added inital api controller, data context and context for seeing the database

--- ProductStore/Global.asax.cs
+++ ProductStore/Global.asax.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -23,6 +22,9 @@ namespace ProductStore
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+
+            System.Data.Entity.Database.SetInitializer(
+                new Models.OrdersContextInitializer());
         }
     }
 }
